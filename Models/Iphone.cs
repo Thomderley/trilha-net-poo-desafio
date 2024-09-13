07d4fbd3e07e0c512842202e8b26e58a7f1d586f
Thomaz Wanderley <thomaz_haw@hotmail.com>
2024-09-12 22:33:04 -0300

    Desafio completado.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+
+        public Iphone ()
+        {
+
+        }
+        public Iphone ( string numero, string modelo, string imei, int memoria ) : base (numero, modelo,  imei, memoria)
+        {
+
+        }
+        
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Iniciando processo de instalação de {nomeApp}");
+        }
     }
 }
